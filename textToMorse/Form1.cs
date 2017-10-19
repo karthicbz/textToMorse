879b0adb28536a8cb6ebf01f706427003b856184
karthicbz <pkarthicbz@live.in>
2017-10-19 15:26:06 +0530

    Add files via upload

---
+++ textToMorse/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textToMorse
+{
+    public partial class Form1 : Form
+    {
+        textToMorse tm;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tm = new textToMorse();
+            tm.textToMorseCode(textBox1.TextLength, textBox1.Text);
+            textBox2.Text = tm.returnMorse(textBox1.TextLength);
+        }
+    }
+}
---
+++ textToMorse/textToMorse.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textToMorse
+{
+    class textToMorse
+    {
+        public char[] text;
+        public string[] Morse = new string[] {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+                                        ".-..", "--", "-.", "---", ".--.", "--.-", "-.-", "...", "-", "..-",
+                                        "...-", ".--", "-..-", "-.--", "--..", "/"};
+        //public string[] MorseCode;
+        public string[] Character = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                                                    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " " };
+        public int[] numbers;
+        public void textToMorseCode(int textLength, string Text)
+        {
+            text = new char[textLength];
+            //MorseCode = new string[textLength];
+            numbers = new int[textLength];
+            for (int i = 0; i < textLength; i++)
+            {
+                text[i] = char.ToLower(Text[i]);
+            }
+
+            for (int j = 0; j < textLength; j++)
+            {
+                int index = Array.FindIndex(Character, x => x == text[j].ToString());
+                numbers[j] = index;
+                /*if (text[j].Equals('a'))
+                {
+                    MorseCode[j] = Morse[0];
+                }
+                else if (text[j].Equals('b'))
+                {
+                    MorseCode[j] = Morse[1];
+                }
+                else if (text[j].Equals('c'))
+                {
+                    MorseCode[j] = Morse[2];
+                }
+                else if (text[j].Equals('d'))
+                {
+                    MorseCode[j] = Morse[3];
+                }
+                else if (text[j].Equals('e'))
+                {
+                    MorseCode[j] = Morse[4];
+                }
+                else if (text[j].Equals('f'))
+                {
+                    MorseCode[j] = Morse[5];
+                }
+                else if (text[j].Equals('g'))
+                {
+                    MorseCode[j] = Morse[6];
+                }*/
+            }
+        }
+
+        public string returnMorse(int charLength)
+        {
+            string MCode = "";
+            for(int l=0; l<charLength; l++)
+            {
+                MCode += " " + Morse[numbers[l]];
+            }
+            return MCode;
+        }
+
+        }
+    }
